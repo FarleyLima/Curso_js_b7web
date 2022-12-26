35c8d61dc9ec7b01c819685de9096d9f9488dd2c
Farley Lima <farleybrisa@gmail.com>
2022-12-26 15:09:20 -0300

    aula tipo de dados

---
+++ tipos_de_dados/script.js
@@ -0,0 +1,8 @@
+let string = 'texto'
+let number = 27
+let bulian = true
+let array = ['chocolate', 10, 'bolachas', 5, 'biscoitos']
+let object = { nome: 'Jack', id: 90, function: 'Killer' }
+
+console.log(string, number, bulian, array, object)
+console.log(typeof number)
