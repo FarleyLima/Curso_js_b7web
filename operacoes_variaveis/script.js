7191ea3a9250c7f02dbefc9f8e91da0a0620b364
Farley Lima <farleybrisa@gmail.com>
2022-12-27 22:24:51 -0300

    fazendo alterações no script.js

--- operacoes_variaveis/script.js
+++ operacoes_variaveis/script.js
@@ -1,1 +1,19 @@
-// Operações com números
+// Operações com números. Exemplo #1
+var number1 = 2
+var number2 = 3
+
+var result1 = number1 + number2
+
+console.log(result1)
+
+// Exemplo #2
+
+var number3 = 20
+var number4 = 65
+var number5 = 17
+
+var result2 = number4 + number5 - number3
+
+console.log(result2)
+
+// Operações com strings
