7dd3ec5d99db16f4021393bddca10f26a7a3f723
Farley Lima <farleybrisa@gmail.com>
2022-12-30 23:11:55 -0300

    Adicionado os arquivo html e js na pasta

---
+++ condicionais_if_else/script.js
@@ -0,0 +1,19 @@
+// Praticando com condicionais simples
+
+var age = 15
+
+if (age >= 18) {
+  console.log('você é MAIOR de idade!')
+} else {
+  console.log('Voçê é MENOR de idade!')
+}
+
+// Exemplo #2
+
+var number = 60
+
+if (number > 40) {
+  console.log('O numero number é MAIOR')
+} else {
+  console.log('O numero number é MENOR')
+}
