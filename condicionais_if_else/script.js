76672698980e185296fc9ee397a6835c0d91aea0
Farley Lima <farleybrisa@gmail.com>
2023-01-04 23:11:13 -0300

    Atualizando o conteúdo, praticando sobre mult-condicionais ( && ) e condicionais dupla ( if else)

--- condicionais_if_else/script.js
+++ condicionais_if_else/script.js
@@ -17,3 +17,25 @@ if (number > 40) {
 } else {
   console.log('O numero number é MENOR')
 }
+
+// MULT-CONDICIONAIS &&
+
+var wege = 1500
+
+if (wege > 1320 && wege < 2000) {
+  console.log('O salário é maior que R$ 1320')
+}
+
+// CONDICIONAL DUPLA ( IF ELSE )
+
+var person1 = 1000
+var person2 = 2500
+var person3 = 3000
+
+if (person1 > person2) {
+  console.log('person1 é mais rica que person2')
+} else if (person2 > person1 && person2 > person3) {
+  console.log('person2 é mais rica que person1 e 3')
+} else if (person3 > person2 && person3 > person1) {
+  console.log(`person3 é mais rico de todos, pois ele tem R$ ${person3}.`)
+}
