0c198004590b7b5260124c5d0881250aeb790fd8
Farley Lima <farleybrisa@gmail.com>
2023-01-26 22:27:59 -0300

    Aula #14 modulo #2 operações com array, práticando

---
+++ introdução_array/script.js
@@ -0,0 +1,27 @@
+// --- CRIANDO UMA LISTA SIMPLES COM ARRAY --- //
+let colors = ['blue', 'red', 'yelon', 'green', 'black']
+
+console.log(colors[0])
+
+// --- NESTE EXEMPLO ESTOU EXIBINDO A LISTA COLORS DENTRO DA ARRAY LIST1 --- //
+
+let list1 = [colors, 'carro', 'moto', 'barco']
+
+console.log(list1[0])
+
+// --- NESTE EXEMPLO ESTOU CRIANDO A LISTA DENTRO DA ARRAY LIST2 --- //
+
+let list2 = ['sabão', ['sabugo', 50, 'R$:300'], 500, 'shampoo']
+
+console.log(list2[1])
+
+// --- NESTE EXEMPLO ESTOU ACESSANDO A LISTA DENTRO DA ARRAY LIST2 E BUSCADO O ITEM "R$:300" --- //
+
+console.log(list2[1][2])
+
+// --- ARMAZENANDO O ITEM 3 " SHAMPOO " DENTRO DE UMA VARIÁVEL --- //
+let opc = list2[3]
+
+console.log(opc)
+
+// ----     OPERAÇÕES BÁSICAS COM ARRAY     ---- //
