2959a4400977bb2a0e9b7bcb538c2f2c064686c5
Farley Lima <farleybrisa@gmail.com>
2023-02-01 23:13:57 -0300

    praticando acesso e alterações em objetos. Aula #17, modulo #2

--- objetos/script.js
+++ objetos/script.js
@@ -51,3 +51,66 @@ console.log(personagem.armas)
 // acessando uma caracteristica de o array ' armas ', dentro do obejeto ' personagem ' \\
 
 console.log(personagem.armas[2])
+
+// ---- ACESSANDO E ALTERANDO OBJETOS ---- \\
+
+// criando obejeto personagem #2
+let personagem2 = {
+  nome: 'Jack',
+  iadade: 20,
+  mundo: 'Marte',
+
+  equipamentos: ['faca militar', 'kit sobrevivência'],
+
+  golpes: {
+    punho: 'suave',
+    chute: 'supremo',
+    energia: 250
+  }
+}
+
+console.log(personagem2)
+
+// alterando o nome do personagem #2. Alterando a o valor da propriedade.
+
+personagem2.nome = 'Jackinho'
+
+console.log(personagem2.nome)
+
+// alterando a energia do personagem #2. Alterando a o valor da propriedade.
+
+personagem2.golpes.energia += 100
+
+console.log(
+  `A energia de ${personagem2.nome} foi aumentada para ${personagem2.golpes.energia}%`
+)
+
+// alterando  o array, adicionado mais opções.
+
+personagem2.equipamentos.push('Barraca')
+
+console.log(personagem2.equipamentos[2])
+
+// ---- PRATICA #2 ---- \\
+
+let loja = {
+  nome: 'FL autos',
+  horaF: 'Das 08:00hrs as 18:00hrs',
+
+  veiculos: [
+    { marca: 'Fiat', modelo: 'Toro ultra', motor: 2.0, valor: 220 },
+    { marca: 'Ford', modelo: 'Ranger Limited', motor: 3.2, valor: 275 },
+    { marca: 'Chevrolet', modelo: 'S10 Z10', motor: 3.2, valor: 285 }
+  ]
+}
+
+// acessando os dados do objeto " loja "
+
+console.log(loja)
+
+console.log(`Nova loja de veículos ${loja.nome}, venha conferir nosso estoque!`)
+
+// acessando e visualizando a lista array e os obejetos dentro dela .
+
+var carro3 = loja.veiculos[1].valor
+console.log(`Chevrolet S10 z10 custa R$ ${carro3} mil. `)
