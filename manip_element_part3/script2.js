ff65689d2d345657128a509c668d8c996cf1a7c6
Farley Lima <farleybrisa@gmail.com>
2023-03-02 21:52:52 -0300

    atualização

--- manip_element_part3/script2.js
+++ manip_element_part3/script2.js
@@ -1,8 +1,8 @@
 function click() {
   const list = document.querySelector('#lista1')
   const list1 = list.querySelector('ul')
-
-  newButton = document.createElement('button')
+  // ADICIONANDO UM NOVO BOTÃO
+  const newButton = document.createElement('button')
   newButton.innerHTML = 'clique aqui!'
 
   list1.before(newButton)
